8e5570c54851a2817e3ad6c85ccedc8f5ce4a508
khmehedihasan <mehedihasan47341@gmail.com>
2022-11-19 23:05:33 +0600

    customer view module added

---
+++ pos/header/modules/customer/viewCustomer.h
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "../../../tabulate/table.hpp"
+using namespace std;
+using namespace tabulate;
+
+    void viewCustomer(){
+
+        customers *data = startC;
+
+        Table movies;
+        movies.add_row({"Id", "Name", "Email", "Phone","Address", "Date"});
+
+        for (size_t i = 0; i < 6; ++i) {
+            movies[0][i].format()
+                .font_color(Color::yellow)
+                .font_align(FontAlign::center)
+                .font_style({FontStyle::bold});
+
+        }
+
+        int n = 0;
+        string total ;
+        while(data != NULL){
+            n++;
+            movies.add_row({to_string(data->id), data->name, data->email, data->phone, data->address, data->date});
+
+            data = data->next;
+        }
+
+        std::cout << movies << std::endl;
+        total = "Total customers: " + to_string(n);
+        successMessage(total);
+
+    }
+
